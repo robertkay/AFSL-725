537d6a6de8d4bd31566379117eb20f6a2850ef06
robertassuredsupport <robert@assuredsupport.com.au>
2024-04-18 06:30:20 +1000

    Added environmental variables for different deployment types for testing in integration
    Changed the VITE config to allow this to occur.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  console.log('Current mode:', mode);
+  console.log('Current Base URL:', env.VITE_BASE_URL);
+  console.log('Current API URL:', env.VITE_API_BASE_URL);
+   
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || './',  // Fallback to './' if VITE_BASE_URL is not defined
+  };
+});
